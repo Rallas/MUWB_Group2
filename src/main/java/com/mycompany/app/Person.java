ba6cad8ab3cfe6a72597f9070f7f1bb15455be9c
Rallas <radioactiveplayer@gmail.com>
2022-11-20 18:02:29 -0600

    LAST OF THE FUNCTIONALITY FOR MINIMAL VERSION CROSS YOUR HEART AND HOPE TO DIE, BEST WORK OR I EXPLODE rest is js/html side

--- src/main/java/com/mycompany/app/Person.java
+++ src/main/java/com/mycompany/app/Person.java
@@ -78,103 +78,120 @@ public class Person extends Actions{
         U.GameId = G.GameId;
         
         //prio goes dealer, cheater, high, mid, low
-        if(this.type == PlayerType.DEALER)
+        if(this.hasWagered == 1)
         {
-            U.Button = 1;
-            G.Update(U); //messy. Allows hit, but attempts to force it. Technically logically sound but don like it one bit
-            while(this.count(this.hand.get(currentDepth)) < 16 && myTurn == 1)
+            if(this.type == PlayerType.DEALER)
             {
                 U.Button = 1;
-                myTurn = G.Update(U);
-            }
-            U.Button = 0;
-            myTurn = G.Update(U);
-        }
-        if(this.type == PlayerType.BOTCHEAT)
-        {
-            U.PlayerId = this.playerID;
-            U.GameId = G.GameId;
-            if(this.count(this.hand.get(currentDepth))>8 && this.count(this.hand.get(currentDepth))<12)
-            {
-                U.Button = 3;
-                myTurn = G.Update(U);
-            }
-            while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
-            {
-                
-                if(this.Split(this.hand.get(currentDepth)) > 0 && splitsRemaining > 0)
+                G.Update(U); //messy. Allows hit, but attempts to force it. Technically logically sound but don like it one bit
+                while(this.count(this.hand.get(currentDepth)) < 16 && myTurn == 1)
                 {
-                    splitsRemaining--;
-                    U.Button = 2;
+                    U.Button = 1;
                     myTurn = G.Update(U);
                 }
-                if(rand.nextInt(3)<3) //even cheaters aren't perfect- 75% chance to 'cheat' a draw
+                U.Button = 0;
+                myTurn = G.Update(U);
+            }
+            if(this.type == PlayerType.BOTCHEAT)
+            {
+                U.PlayerId = this.playerID;
+                U.GameId = G.GameId;
+                if(this.count(this.hand.get(currentDepth))>8 && this.count(this.hand.get(currentDepth))<12)
                 {
-                    U.Button = 99; //special button code that corresponds to a 'cheat hit' --not present on the UI
+                    U.Button = 3;
                     myTurn = G.Update(U);
                 }
-                else
+                while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
+                {
+                    
+                    if(this.Split(this.hand.get(currentDepth)) > 0 && splitsRemaining > 0)
+                    {
+                        splitsRemaining--;
+                        U.Button = 2;
+                        myTurn = G.Update(U);
+                    }
+                    if(rand.nextInt(3)<3) //even cheaters aren't perfect- 75% chance to 'cheat' a draw
+                    {
+                        U.Button = 99; //special button code that corresponds to a 'cheat hit' --not present on the UI
+                        myTurn = G.Update(U);
+                    }
+                    else
+                    {
+                        U.Button = 1; //regular hit
+                        myTurn = G.Update(U);
+                    }
+                }
+                U.Button = 0;
+                myTurn = G.Update(U);
+            }
+            if(this.type == PlayerType.BOTHIGH)
+            {
+                U.PlayerId = this.playerID;
+                U.GameId = G.GameId;
+                if(this.count(this.hand.get(currentDepth))>8 && this.count(this.hand.get(currentDepth))<12)
+                {
+                    U.Button = 3;
+                    myTurn = G.Update(U);
+                }
+                while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
+                {
+                    
+                    if(this.Split(this.hand.get(currentDepth)) > 0 && splitsRemaining > 0)
+                    {
+                        splitsRemaining--;
+                        U.Button = 2;
+                        myTurn = G.Update(U);
+                    }
+                    U.Button = 1; //regular hit
+                    myTurn = G.Update(U);
+                }
+                U.Button = 0;
+                myTurn = G.Update(U);
+            }
+            if(this.type == PlayerType.BOTMID)
+            {
+                U.PlayerId = this.playerID;
+                U.GameId = G.GameId;
+                while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
+                {
+                    
+                    if(this.Split(this.hand.get(currentDepth)) > 0 && splitsRemaining > 0)
+                    {
+                        splitsRemaining--;
+                        U.Button = 2;
+                        myTurn = G.Update(U);
+                    }
+                    U.Button = 1; //regular hit
+                    myTurn = G.Update(U);
+                }
+                U.Button = 0;
+                myTurn = G.Update(U);
+            }
+            if(this.type == PlayerType.BOTLOW)
+            {
+                U.PlayerId = this.playerID;
+                U.GameId = G.GameId;
+                while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
                 {
                     U.Button = 1; //regular hit
                     myTurn = G.Update(U);
                 }
+                U.Button = 0;
+                myTurn = G.Update(U);
             }
-            U.Button = 0;
-            myTurn = G.Update(U);
+            
         }
-        if(this.type == PlayerType.BOTHIGH)
+        else
+        if(this.hasWagered == 0)
         {
-            U.PlayerId = this.playerID;
-            U.GameId = G.GameId;
-            if(this.count(this.hand.get(currentDepth))>8 && this.count(this.hand.get(currentDepth))<12)
+            if(this.type == PlayerType.DEALER)
             {
-                U.Button = 3;
-                myTurn = G.Update(U);
+                U.Button = 0;
             }
-            while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
+            else
             {
-                
-                if(this.Split(this.hand.get(currentDepth)) > 0 && splitsRemaining > 0)
-                {
-                    splitsRemaining--;
-                    U.Button = 2;
-                    myTurn = G.Update(U);
-                }
-                U.Button = 1; //regular hit
-                myTurn = G.Update(U);
+                U.Button = (this.winnings * 1/3) + rand.nextInt(this.winnings * 2/3);
             }
-            U.Button = 0;
-            myTurn = G.Update(U);
-        }
-        if(this.type == PlayerType.BOTMID)
-        {
-            U.PlayerId = this.playerID;
-            U.GameId = G.GameId;
-            while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
-            {
-                
-                if(this.Split(this.hand.get(currentDepth)) > 0 && splitsRemaining > 0)
-                {
-                    splitsRemaining--;
-                    U.Button = 2;
-                    myTurn = G.Update(U);
-                }
-                U.Button = 1; //regular hit
-                myTurn = G.Update(U);
-            }
-            U.Button = 0;
-            myTurn = G.Update(U);
-        }
-        if(this.type == PlayerType.BOTLOW)
-        {
-            U.PlayerId = this.playerID;
-            U.GameId = G.GameId;
-            while(this.count(this.hand.get(currentDepth)) < this.agression && myTurn == 1)
-            {
-                U.Button = 1; //regular hit
-                myTurn = G.Update(U);
-            }
-            U.Button = 0;
             myTurn = G.Update(U);
         }
         return 0;
